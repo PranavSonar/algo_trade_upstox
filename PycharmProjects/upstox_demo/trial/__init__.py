b58b0a0a8e6ba68098b9aae5ca0a1320f51e1274
Amit <amit.sharma4@volkswagen.co.in>
2017-12-04 19:37:13 +0530

    finally working nifty vwap

--- PycharmProjects/upstox_demo/trial/__init__.py
+++ PycharmProjects/upstox_demo/trial/__init__.py
@@ -1,9 +1,9 @@
 from upstox_api.api import *
 
 
-s = Session('vsmym5Dcv13SJVZT0yhqA5lfXDHakUJt3ShWj5bd')
-s.set_redirect_uri('http://127.0.0.1')
-s.set_api_secret('2vf3l4vcgi')
+s = Session('JMJueJYcG05yrycTa4tBX3zP5KGqOl2c8dAZ31Ay')
+s.set_redirect_uri('http://127.0.0.0')
+s.set_api_secret('on8l8gvped')
 
 
 # use below url to login and get code
@@ -11,7 +11,7 @@ print (s.get_login_url())
 
 
 # # code goes here
-s.set_code ('ea994b10d7afa24a6c0753754f28d46ec73f0bf8')
+s.set_code ('bd2660401135803d826776b4bd21bf6f03e7031c')
 
 # use code to generate access token
 access_token = s.retrieve_access_token()
--- PycharmProjects/upstox_demo/trial/niftyVWAP.py
+++ PycharmProjects/upstox_demo/trial/niftyVWAP.py
@@ -3,31 +3,36 @@ from datetime import datetime
 from threading import Timer
 
 #Api key , access token
-u = Upstox ('vsmym5Dcv13SJVZT0yhqA5lfXDHakUJt3ShWj5bd', '8fb958b52437c680083fbff877f05057be42d848')
+u = Upstox ('JMJueJYcG05yrycTa4tBX3zP5KGqOl2c8dAZ31Ay', '1cc0d35294ab77715886a44eee604907c63de010')
 
 u.get_master_contract('NSE_FO') # get contracts for NSE FO
 u.get_master_contract('NSE_INDEX') # get contracts for NSE_INDEX
 
 
 india_vix_nse_index = u.get_instrument_by_symbol('NSE_INDEX', 'INDIA_VIX')
-nifty_nse_future = u.get_instrument_by_symbol('NSE_FO', 'nifty17novfut')
+nifty_nse_future = u.get_instrument_by_symbol('NSE_FO', 'nifty17decfut')
 
 w=datetime.today()
 
-x=w.replace(day=w.day + 1, hour=9, minute=14, second=0, microsecond=0)
-y=w.replace(day=w.day + 1, hour=9, minute=14, second=30, microsecond=0)
-z=w.replace(day=w.day + 1, hour=9, minute=15, second=5, microsecond=0)
+x=w.replace(day=w.day + 1, hour=9, minute=14, second=1, microsecond=0)
+y=w.replace(day=w.day + 1, hour=9, minute=15, second=10, microsecond=0)
+z=w.replace(day=w.day + 1, hour=9, minute=15, second=15, microsecond=0)
 
 delta_t= x - w
 delta_t_calculation = y - w
 delta_t_order_place = z - w
 time_for_open_prices= delta_t.seconds + 1
-time_for_calculation = delta_t_calculation + 1
-time_for_order = delta_t_order_place+1
+time_for_calculation = delta_t_calculation.seconds + 1
+time_for_order = delta_t_order_place.seconds+1
 
 india_vix_open=0
+nifty_o= 0
+buy_above=0
+buy_target=0
+sl_for_sell=0
+sl_for_buy=0
+sell_target=0
 
-nifty_open=0
 def fetch_open_prices():
     india_vix = u.get_live_feed(india_vix_nse_index, LiveFeedType.Full)
     global india_vix_open
@@ -37,11 +42,7 @@ def fetch_open_prices():
     nifty_open = nifty_nse_future_full["open"]
 
 
-buy_above=0
-buy_target = 0
-sl_for_sell = 0
-sl_for_buy=0
-sell_target = 0
+
 def calculate_target_and_sl():
     first_sd = nifty_open * india_vix_open * 0.00104684784518
     global buy_above
@@ -56,10 +57,31 @@ def calculate_target_and_sl():
     sell_target = nifty_open - 0.382 * first_sd
     global sl_for_sell
     sl_for_sell = nifty_open + 1
+    print 'buy_above : '  + buy_above
+    print 'buy_target : '  + buy_target
+    print 'sl_for_sell : '  + sl_for_sell
+    print 'sl_for_buy : '  + sl_for_buy
+    print 'sell_target : '  + sell_target
 
 
-def place_nifty_order():
-    t=0
+
+
+def place_nifty_buy_order():
+    u.place_order(TransactionType.Buy,  # transaction_type
+                  nifty_nse_future,  # instrument
+                  1,  # quantity
+                  OrderType.Limit,  # order_type
+                  ProductType.Intraday,  # product_type
+                  buy_above,  # price
+                  None,  # trigger_price
+                  0,  # disclosed_quantity
+                  DurationType.DAY,  # duration
+                  sl_for_buy,  # stop_loss
+                  buy_target,  # square_off
+                  None)  # trailing_ticks
+
+
+
 
 open_price_timer = Timer(time_for_open_prices, fetch_open_prices)
 open_price_timer.start()
@@ -67,20 +89,5 @@ open_price_timer.start()
 target_set_timer = Timer(time_for_calculation, calculate_target_and_sl)
 target_set_timer.start()
 
-u.place_order(TransactionType.Buy,  # transaction_type
-              u.get_instrument_by_symbol('NSE_FO', 'UNITECH'),  # instrument
-              1,  # quantity
-              OrderType.Limit,  # order_type
-              ProductType.Delivery,  # product_type
-              8.0,  # price
-              None,  # trigger_price
-              0,  # disclosed_quantity
-              DurationType.DAY,  # duration
-              None,  # stop_loss
-              None,  # square_off
-              None)  # trailing_ticks
-
-
-
-
-# print first_sd
+place_order_set_timer = Timer(time_for_order, place_nifty_buy_order)
+place_order_set_timer.start()
